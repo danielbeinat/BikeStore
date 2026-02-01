bc5b389b4c624a397fc4b89d3339ec48ad7abb5f
danielbeinat <danielbeinat@outlook.es>
2026-01-31 21:20:45 -0300

    update

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true,
-    domains: [],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        unoptimized: true,
+        domains: [],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+            },
+        ],
+    },
+    compiler: {
+        removeConsole: process.env.NODE_ENV === 'production',
+    },
 }
 
 module.exports = nextConfig
